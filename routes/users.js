7b9f63204a5e163fa1be5f438300d70c5699f1a3
abhilash <abhilash.kamble376@gmail.com>
2021-09-09 13:28:29 +0530

    update

---
+++ routes/users.js
@@ -0,0 +1,115 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../model/user");
+const app = express();
+const serverless = require("serverless-http");
+
+router.get('/', async (req, res) => {
+
+    console.log("Users Get Request is Called");
+
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        console.error("Error is " + error);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+
+    console.log("Users Get Request is Called for ID =" + req.params.id);
+
+    try {
+        const user = await User.findById(req.params.id);
+        res.json(user);
+    } catch (error) {
+        console.error("Error is " + error);
+        res.send("Error is " + error)
+    }
+});
+
+router.post('/signup', async (req, res) => {
+
+    const user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        mobileNo: req.body.mobileNo,
+        password: req.body.password,
+        loginAttempt: req.body.loginAttempt
+    });
+
+    try {
+        const a1 = await user.save();
+        res.json(a1);
+    } catch (error) {
+        console.error("Error is " + error);
+        res.send("<h1>Error while fetching the Users</h1><p>" + error + "</p>");
+    }
+
+});
+
+
+
+router.patch('/:id', async (req, res) => {
+
+    try {
+        const user = await User.findById(req.params.id);
+        user.mobileNo = req.body.mobileNo;
+        const u1 = await user.save();
+        res.json(u1);
+    } catch (error) {
+        console.error("Error is " + error);
+        res.send("<h1>Error while fetching the Users</h1><p>" + error + "</p>");
+    }
+
+});
+
+
+router.post('/signin', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({
+                error: true,
+                message: "Cannot authorize user.",
+            });
+        }
+        //1. Find if any account with that email exists in DB
+        const user = await User.findOne({ email: email });
+        // NOT FOUND - Throw error
+        if (!user) {
+            return res.status(404).json({
+                error: true,
+                message: "Account not found",
+            });
+        }
+
+        //3. Verify the password is valid
+        const isValid = await User.findOne({ password: password });
+        if (!isValid) {
+            return res.status(400).json({
+                error: true,
+                message: "Invalid credentials",
+            });
+        }
+        await user.save();
+
+        //Success
+        return res.send({
+            success: true,
+            message: "User logged in successfully",
+        });
+    } catch (err) {
+        console.error("Login error", err);
+        return res.status(500).json({
+            error: true,
+            message: "Couldn't login. Please try again later.",
+        });
+    }
+});
+
+
+module.exports = router;
+module.exports.server = serverless(app);
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+// const url = "mongodb://localhost/UrbanFarmerDB";
+const url = `mongodb+srv://admin:admin@cluster0.kszi7.mongodb.net/urbanfarmer?retryWrites=true&w=majority`
+const app = express();
+const morgan = require('morgan')
+app.use(morgan('combined'))
+const serverless = require("serverless-http");
+const router = express.Router();
+const userRouter = require("./routes/users");
+
+mongoose.connect(url, { useNewUrlParser: true });
+const con = mongoose.connection;
+
+con.on('open', function () {
+    console.log("mongo is running .. 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 ");
+})
+
+//Middleware to be impored and used
+app.use(express.json());
+app.use(cors());
+
+// router.get("/", (req, res) => {
+//     res.json({
+//       hello: "hi!"
+//     });
+//   });
+
+app.use('/users', userRouter);
+
+app.use(`/api`, router);
+
+app.listen(4000, () => {
+    console.log("The Server is running on http://localhost:9000");
+});
+
+app.get('/', async (req, res, next) => {
+    res.status(200).send('Hello World! from index')
+})
+
+
+// module.exports = app;
+
+module.exports.server = serverless(app);
+
